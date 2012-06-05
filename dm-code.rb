1c1698a0a2dd94c97184211b523539233488b53b
Jason Laster <Jason.laster.11@gmail.com>
2012-06-05 17:42:48 -0400

    add first lesson

--- dm-code.rb
+++ dm-code.rb
@@ -1,2 +1,6 @@
 require 'rubygems'
 require 'data_mapper'
+
+# setup sqlite3 connections
+DataMapper.setup(:default, 'sqlite::memory:')
+
--- dm-spec.rb
+++ dm-spec.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require_relative 'dm-code'
 
 
@@ -15,28 +16,55 @@ describe "Lesson 1 -- Getting Setup" do
   # First, you will need to install an Adapter, which allows DataMapper to communicate to the Database:
   # http://datamapper.org/getting-started.html
   it "Step 1 -- Install Adapter" do
-    adapter = `gem list | grep .+-adapter`
+    gems = `gem list | grep ^dm-[a-zA-Z]*-adapter`
+    gems.should_not be_empty
   end
 
   # # 2. Install DataMapper
   # If you have RubyGems installed, open a Terminal and install a few things.
   # gem install data_mapper
   it "Step 2 -- Install DataMapper" do
-    dm = `gem list | grep data_mapper`
-    dm.should match(/data_mapper/)
+    data_mapper_gem = `gem list | grep data_mapper`
+    data_mapper_gem.should_not be_empty
   end
   
-  # # 3. Require it in your application
-  it "Step 3" do
-    DataMapper.should exist
+  # # 3. Require DataMapper in your application
+  # This is the first time we'll want to add code to dm-code.rb
+  # if DataMapper does not exist, you probably forgot to require it! 
+  it "Step 3 -- require DataMapper" do
+    Kernel.const_get(:DataMapper).should == DataMapper
   end
   
-  
-  
-  
-  
-  
+  # # 4. Specify your database connection
+  # You need to make sure to do this before you use your models, i.e. before you actually start accessing the database.
+  # You will need to create a database and supply a path. 
+  # If you are using sqlite3,  the  puppies.db
+  it "Step 4 -- Specify your database connection" do
+    repo = DataMapper.repository(:default)
+    repo.should be_instance_of DataMapper::Repository
+  end
+end
 
 
+describe "Lesson 2 -- Define a Puppy" do
+  
+  # # 5. Define models and properties
+  # The Post model is going to need to be persistent, so we'll include DataMapper::Resource. The convention with model names is to use the singular, not plural version...but that's just the convention, you can do whatever you want.
+  it "Step 5 -- Define models and properties" do 
+  end
+  
+  # # 6. Setup associations
+  it "Step 6 -- Setup associations"
 
+  # # 7. finalize models
+  it "7 -- finalize models"
+  
+  # # 8. Setup Tables in Database
+  it "8 -- Setup Tables in Database"
+  
+  # # 9. Create resource object
+  it "9 -- Create resource object"
+  
+  # # 10. Query the database
+  it "10 -- Query the database"
 end
