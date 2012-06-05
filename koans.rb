74ba11bd3088dc2ec272bfe68453b5b3a4e74f24
Jason Laster <Jason.laster.11@gmail.com>
2012-06-05 17:42:27 -0400

    refactor koans

--- koans.rb
+++ koans.rb
@@ -1,18 +1,27 @@
 require 'pry'
 require "yaml"
 
-output_msg = `rspec test.rb --require ./koans/abort_on_first_failure_formatter.rb --format AbortOnFirstFailureFormatter`
 
-failed_test = YAML::load(output_msg)
 
-message = "You have done well."
 
-if failed_test
-  lesson = failed_test[:lesson]
-  step = failed_test[:step]
-  exception = failed_test[:exception]
-  
-  message = "In #{lesson}, on #{step}, an error of #{exception} occured"
+def run_rspec(file)
+  output_msg = `rspec #{file} --require ./koans/abort_on_first_failure_formatter.rb --format AbortOnFirstFailureFormatter`
+  YAML::load(output_msg)
 end
 
+def output_message(failed_test)
+  if failed_test
+    lesson = failed_test[:lesson]
+    step = failed_test[:step]
+    exception = failed_test[:exception]
+    message = "#{lesson}, on #{step} \n#{exception}"
+  else
+    message = "You have done well."
+  end
+end
+
+
+failed_test = run_rspec("dm-spec.rb")
+message = output_message(failed_test)
 puts message
+
